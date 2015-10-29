a9f2de7c9d09415bdd5651d0e7283e019d1ad5f0
SineObama <714186139@qq.com>
2015-10-29 10:42:28 +0800

    Signed-off-by: SineObama <714186139@qq.com>

---
+++ c++/sine/Sqrt.h
@@ -0,0 +1,47 @@
+/*
+ * Sqrt.h
+ *
+ *  Created on: 2015Äê10ÔÂ24ÈÕ
+ *      Author: Sine
+ */
+
+#ifndef SQRT_H_
+#define SQRT_H_
+
+namespace Sine {
+
+/*
+ * base on basic operators on class T, also constructor with an integer.
+ */
+
+template<class T>
+T Sqrt2(T _b, int _n, int t = 50) {
+    T src(_b);
+    while (t--) {
+        T tem(_b);
+        for (int j = 0; j < _n - 2; j++)
+            tem = tem * _b;
+        _b = _b - (tem * _b - src) / (T(_n) * tem);
+    }
+    return _b;
+}
+
+template<class T>
+T Sqrt1(T _b, int _n, const T & _m) {
+    T src(_b);
+    int t = 10000;
+    while (t--) {
+        T tem(_b);
+        for (int j = 0; j < _n - 2; j++)
+            tem = tem * _b;
+        _b = _b - (tem * _b - src) / (T(_n) * tem);
+        if ((tem * _b - src > T(0) && tem * _b - src < _m)
+                || (src - tem * _b > T(0) && src - tem * _b < _m))
+            return _b;
+    }
+    return _b;
+}
+
+}
+
+#endif /* SQRT_H_ */
